f76efbec988d440f3011fb11986b14ccb9f00761
pcze <pawel.czerwinski@inetum.com>
2022-06-14 07:47:31 +0200

    add empty main component

---
+++ furniture-orders-manager-front/src/app/main-module/main-module.module.ts
@@ -0,0 +1,15 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {MainComponentComponent} from './main-component/main-component.component';
+
+
+@NgModule({
+  declarations: [
+    MainComponentComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class MainModuleModule {
+}
