f92242b6690831d8cd06fa3bd9a2c49f5a97ae33
pcze <pawel.czerwinski@inetum.com>
2022-06-15 08:56:17 +0200

    Add simple error handling (show in alert and console)  for material

--- FurnitureWithComponentsRepository/src/main/java/com/pawel/furniturewithcomponentsrepository/domain/material/controller/MaterialController.java
+++ FurnitureWithComponentsRepository/src/main/java/com/pawel/furniturewithcomponentsrepository/domain/material/controller/MaterialController.java
@@ -1,11 +1,13 @@
 package com.pawel.furniturewithcomponentsrepository.domain.material.controller;
 
 import com.pawel.furniturewithcomponentsrepository.domain.material.controller.request.AddMaterialRequest;
+import com.pawel.furniturewithcomponentsrepository.domain.material.exceptions.MaterialAlreadyExistsException;
 import com.pawel.furniturewithcomponentsrepository.domain.material.model.Material;
 import com.pawel.furniturewithcomponentsrepository.domain.material.service.MaterialService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,7 +18,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -28,11 +29,19 @@ public class MaterialController {
 
     private final MaterialService service;
 
-    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
-    public void saveMaterial(@Valid @RequestBody AddMaterialRequest request) {
+    public ResponseEntity<?> saveMaterial(@RequestBody AddMaterialRequest request) { //TODO add validator for request
         Material material = new Material(null, request.getName());
-        service.save(material);
+        try {
+            service.save(material);
+        } catch (MaterialAlreadyExistsException me) {
+            me.printStackTrace();
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(me.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        }
+        return ResponseEntity.status(HttpStatus.CREATED).body("Material " + request.getName() + " successfully created.");
     }
 
     @GetMapping
--- furniture-orders-manager-front/src/app/main-module/furniture-manager/material/material.component.ts
+++ furniture-orders-manager-front/src/app/main-module/furniture-manager/material/material.component.ts
@@ -2,7 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {Material} from "./model/Material";
 import {MaterialService} from "./material.service";
 import {CreateMaterialRequest} from "./model/CreateMaterialRequest";
-import {FormControl} from "@angular/forms";
+import {HttpErrorResponse} from "@angular/common/http";
+import {throwError} from "rxjs";
 
 @Component({
   selector: 'app-material',
@@ -13,7 +14,6 @@ export class MaterialComponent implements OnInit {
 
 
   materials: Material[] = [];
-  materialNameFormCtrl = new FormControl('');
 
   constructor(private materialService: MaterialService) {
   }
@@ -36,9 +36,30 @@ export class MaterialComponent implements OnInit {
     let request: CreateMaterialRequest = {
       name: name
     };
-    this.materialService.saveMaterial(request).subscribe(mat => {
-      this.materials.push(mat);
-      this.loadMaterials();
-    });
+    this.materialService.saveMaterial(request).subscribe( //TODO subscribe deprecated
+      () => {
+        this.loadMaterials();
+      },
+      (error) => {
+        console.error(error);
+        this.handleError(error);
+        this.loadMaterials();
+      })
+  }
+
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 0) {
+      console.error('An error occurred:', error.error);
+    } else {
+      if (error.status >= 200 && error.status <= 299) {
+        alert("Material created successfully.")
+        return;
+      }
+      console.error(
+        `Backend returned code ${error.status}, body was: `, error.error);
+      alert('Error! Code: ' + error.status + ' -> ' + error.error)
+    }
+    return throwError(() => new Error('Something bad happened; please try again later.'));
   }
 }
